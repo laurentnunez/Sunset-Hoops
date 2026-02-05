6c18a11e44dd09b3960fdd8a12bddb0358700088
Laurent Nunez <94831096+laurentnunez@users.noreply.github.com>
2026-02-05 16:09:48 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -95,6 +95,23 @@ function renderScoreButtons(){
 
 /* Charge les scores du jour demandé */
 async function loadScoresByDate(dateISO){
+
+import { NBA_LOGOS } from "./logos.js";
+
+function enrichGamesWithLogos(games) {
+  return games.map((g) => {
+    const homeAbbr = g.home_team?.abbreviation;
+    const visitorAbbr = g.visitor_team?.abbreviation;
+
+    return {
+      ...g,
+      homeLogo: NBA_LOGOS[homeAbbr] || null,
+      visitorLogo: NBA_LOGOS[visitorAbbr] || null,
+    };
+  });
+}
+
+
   const list=$("#scores-list");
   const status=$("#scores-status");
   list.innerHTML="";
